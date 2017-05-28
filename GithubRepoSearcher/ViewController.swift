0dbca3f726807932e4351da1f9d2e39fe9d57d47
Yuki Yoshioka <rikusouda@users.noreply.github.com>
2017-05-28 11:32:35 +0900

    Initial Commit

---
+++ GithubRepoSearcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GithubRepoSearcher
+//
+//  Created by Yuki Yoshioka on 2017/05/28.
+//  Copyright © 2017年 rikusouda. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
